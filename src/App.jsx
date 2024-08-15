39de2187a4f7213d89600fd0879d0e7acbb86124
Alina Nikeru <alina.nikeru@gmail.com>
2024-08-15 20:32:44 +0300

    commit
    add store

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar'
+import CardItem from './components/CardItem'
+import Modal from './components/Modal'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>My firs React</h1>
+      <Navbar/>
+      <CardItem/>
+      <Modal/>
     </>
   )
 }
